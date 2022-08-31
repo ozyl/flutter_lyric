beaf643858fb71aa41da89c736577f0039f44bb7
周永乐 <zylvip@163.com>
2022-08-31 13:55:27 +0800

    generate code

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <audioplayers_windows/audioplayers_windows_plugin.h>
-#include <flutter_lyric/flutter_lyric_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
-  FlutterLyricPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterLyricPlugin"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
-  flutter_lyric
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
